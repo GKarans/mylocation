f3012293742fb7486154627c8d1d7f5244d4441c
Guntars <guntars.karans@gmail.com>
2023-11-14 19:59:43 +0200

    Update server.js and add index.html

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('public'));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route for the root path ("/")
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 io.on('connection', (socket) => {
     console.log('A user connected');
