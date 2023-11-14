c29df6fd1678a38f79178a64b694fced7a1e2f77
Guntars <guntars.karans@gmail.com>
2023-11-14 20:10:18 +0200

    server

--- server.js
+++ server.js
@@ -7,31 +7,34 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files from the 'build' directory
-app.use(express.static(path.join(__dirname, 'build')));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Define a route for the root path ("/")
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    // Handle 'updateLocation' event
-    socket.on('updateLocation', (location) => {
-        // Process the updated location
-        console.log('Received location update:', location);
+    // Check if the socket object is defined
+    if (socket) {
+        // Handle 'updateLocation' event
+        socket.on('updateLocation', (location) => {
+            // Process the updated location
+            console.log('Received location update:', location);
 
-        // Broadcast the updated location to other users
-        socket.broadcast.emit('locationUpdated', location);
-    });
+            // Broadcast the updated location to other users
+            socket.broadcast.emit('locationUpdated', location);
+        });
 
-    // Other socket event handlers...
+        // Other socket event handlers...
 
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+        socket.on('disconnect', () => {
+            console.log('A user disconnected');
+        });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
